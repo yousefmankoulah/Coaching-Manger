98c61724fa868aea10682be60a756f4e8c2daff3
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-02-22 17:22:04 +0000

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import ScrollToTop from './components/ScrollToTop';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
@@ -12,7 +12,7 @@ import { SignUp } from './pages/SignUp';
 export default function App() {
   return (
     <BrowserRouter>
-      
+       <ScrollToTop />
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
