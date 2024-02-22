9e00f80b6c64adfd20646f37f54bc9b090dd4cd3
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-02-22 21:37:39 +0000

    diet

--- api/controllers/dietController.js
+++ api/controllers/dietController.js
@@ -1,3 +1,4 @@
+import { Customer } from '../models/customerModel.js';
 import Diet from '../models/dietModel.js';
 import { errorHandler } from '../utils/error.js';
 
@@ -9,8 +10,14 @@ export const createDiet = async (req, res, next) => {
         next(errorHandler(400, 'All fields are required'));
     }
 
+    
     const userId = req.params.userId
     const customerId = req.params.customerId
+    
+    
+    if (req.user.id !== userId) {
+        next(errorHandler(401, 'Unauthorized'));
+    }
 
     const newDiet = new Diet({
         userId: userId,
@@ -34,7 +41,7 @@ export const createDiet = async (req, res, next) => {
 export const getDietCustomerSide = async (req, res, next) => {
     try {
         const diet = await Diet.find({ customerId: req.params.customerId });
-        res.status(200).json(breakfast);
+        res.status(200).json(diet);
     } catch (error) {
         next(error);
     }
@@ -50,4 +57,12 @@ export const getAllDietCoachSide = async (req, res, next) => {
     }
 }
 
-get a diet by id
+
+export const getADietCoachSide = async (req, res, next) => {
+    try {
+        const breakfast = await Diet.findOne({ userId: req.params.userId, _id: req.params._id });
+        res.status(200).json(breakfast);
+    } catch (error) {
+        next(error);
+    }
+}
--- api/routes/dietRoute.js
+++ api/routes/dietRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js'
-import {createDiet, getDietCustomerSide, getAllDietCoachSide} from '../controllers/dietController.js';
+import {createDiet, getDietCustomerSide, getAllDietCoachSide, getADietCoachSide} from '../controllers/dietController.js';
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/createDiet/:userId/:customerId', verifyToken, createDiet);
 router.get('/getDiet/:customerId', verifyToken, getDietCustomerSide);
 router.get('/getAllDiet/:userId', verifyToken, getAllDietCoachSide);
+router.get('/getADiet/:userId/:_id', verifyToken, getADietCoachSide);
 
 
 export default router
