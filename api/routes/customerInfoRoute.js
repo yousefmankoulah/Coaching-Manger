effe311cf974c51487d876cdfbaabbbeeb2b9b23
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-02-23 18:23:47 +0000

    update

--- api/routes/customerInfoRoute.js
+++ api/routes/customerInfoRoute.js
@@ -4,12 +4,12 @@ import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router();
 
-
-router.post('/addCustomerInfo/:customerId', verifyToken, addCustomerInfo);
+//Not tested yet
+router.post('/addCustomerInfo/:userId/:customerId', verifyToken, addCustomerInfo);
 router.get('/getCustomerInfo/:customerId', verifyToken, getCustomerInfo);
 router.put('/updateCutomerInfo/:customerId/:_id', verifyToken, updateCutomerInfo);
 
-router.post('/addCustomerExerciesInfo/:customerId', verifyToken, addCustomerExerciesInfo);
+router.post('/addCustomerExerciesInfo/:userId/:customerId/:setExerciesToCustomerId', verifyToken, addCustomerExerciesInfo);
 router.get('/getAllCustomerExerices/:customerId', verifyToken, getAllCustomerExerices);
 router.get('/getCustomerExerciesBySetExerciesId/:customerId/:setExerciesToCustomerId', verifyToken, getCustomerExerciesBySetExerciesId);
 router.put('/updateCustomerExerciesInfo/:customerId/:_id', verifyToken, updateCustomerExerciesInfo);
