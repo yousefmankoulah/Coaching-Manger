e0ee3f620e817ec564854647c373c0bfc7b3a3d6
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-03-25 20:40:46 +0000

    u

--- client/src/pages/CustomerDetailCoachSide.jsx
+++ client/src/pages/CustomerDetailCoachSide.jsx
@@ -99,14 +99,14 @@ export function CustomerDetailCoachSide() {
                     >
                       Edit
                     </a>
-                    <Button><span
-                    onClick={() => {
-                      setShowModal(true);
-                    }}
-                    className="font-medium text-red-500 hover:underline cursor-pointer"
-                  >
-                    Delete
-                  </span></Button>
+                    <a
+                      onClick={() => {
+                        setShowModal(true);
+                      }}
+                      className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    >
+                      Delete
+                    </a>
                     <a
                       href="#"
                       className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
