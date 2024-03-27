5eccdb21fbe40c7c1c95479cda4f88e3c04b57eb
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-03-27 19:48:00 +0000

    u

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -37,7 +37,9 @@ export function Dashboard() {
       }
     };
 
-    fetchCustomers();
+    if (currentUser?.role === "coach") {
+      fetchCustomers();
+    }
   }, [currentUser]);
 
   const handleCardClick = (cardName) => {
@@ -47,54 +49,66 @@ export function Dashboard() {
   return (
     <div className="min-h-screen mt-20">
       <h1 className="text-3xl font-bold text-center mb-20">
-        Welcome to Dashboard
+        Welcome to the Dashboard
       </h1>
+      {currentUser?.role === "coach" ? (
+        <>
+          <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+            <Card
+              className="max-w-sm"
+              onClick={() => handleCardClick("customers")}
+            >
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Number of Customers
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                You got{" "}
+                {customers && customers.length > 0 ? (
+                  <>
+                    <span className="font-bold">{customers.length}</span>{" "}
+                    customers created
+                  </>
+                ) : (
+                  <>0 customers created</>
+                )}
+              </p>
+              <Button>
+                <Link to="/add-customer">
+                  <span>Add a Client</span>
+                </Link>
+              </Button>
+            </Card>
 
-      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-        <Card className="max-w-sm" onClick={() => handleCardClick("customers")}>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Number of Customers
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            You got{" "}
-            {customers && customers.length > 0 ? (
-              <>
-                <span className="font-bold">{customers.length}</span> customers
-                created
-              </>
-            ) : (
-              <>0 customers created</>
-            )}
-          </p>
-          <Button>
-            <Link to="/add-customer">
-              <span>Add a Client</span>
-            </Link>
-          </Button>
-        </Card>
+            <Card
+              className="max-w-sm"
+              onClick={() => handleCardClick("exercises")}
+            >
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Number of Exercises
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                You got 10 Exercises created
+              </p>
+            </Card>
 
-        <Card className="max-w-sm" onClick={() => handleCardClick("exercises")}>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Number of Exercises
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            You got 10 Exercises created
-          </p>
-        </Card>
+            <Card className="max-w-sm" onClick={() => handleCardClick("diet")}>
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Number of Diet Plans
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                You got 10 Diet Plans created
+              </p>
+            </Card>
+          </div>
 
-        <Card className="max-w-sm" onClick={() => handleCardClick("diet")}>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Number of Diet Plans
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            You got 10 Diet Plans created
-          </p>
-        </Card>
-      </div>
-      {selectedCard === null && <CustomersTable />}
-      {selectedCard === "customers" && <CustomersTable />}
-      {selectedCard === "exercises" && <ExercisesTable />}
-      {selectedCard === "diet" && <DietPlansTable />}
+          {selectedCard === null && <CustomersTable />}
+          {selectedCard === "customers" && <CustomersTable />}
+          {selectedCard === "exercises" && <ExercisesTable />}
+          {selectedCard === "diet" && <DietPlansTable />}
+        </>
+      ) : (
+        <div>Tmam</div>
+      )}
     </div>
   );
 }
