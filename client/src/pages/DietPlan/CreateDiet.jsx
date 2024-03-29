c62ad16f9974d029fe797b4ab486c6fba19de0cf
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-03-29 15:31:25 +0000

    u

--- client/src/pages/DietPlan/CreateDiet.jsx
+++ client/src/pages/DietPlan/CreateDiet.jsx
@@ -127,21 +127,26 @@ export default function CreateDiet() {
               <Label value="Choose the Customer Name" />
 
               <select
+                required
                 id="customerId"
                 onChange={handleChange}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
-                <option value="">Choose the Customer Name</option>
                 {customer && customer.length > 0 ? (
-                  customer.map((customers) => (
-                    <>
-                      <option value={customers._id}>
+                  <>
+                    <option value="" disabled selected>
+                      Choose the Customer Name
+                    </option>
+                    {customer.map((customers) => (
+                      <option key={customers._id} value={customers._id}>
                         {customers.customerName}
                       </option>
-                    </>
-                  ))
+                    ))}
+                  </>
                 ) : (
-                  <option selected>There is No customers</option>
+                  <option value="" disabled selected>
+                    There are no customers
+                  </option>
                 )}
               </select>
             </div>
