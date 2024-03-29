e1acea1bb98ee8c1f114b818ca667c41b7cdeecd
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-03-29 12:49:33 +0000

    u

--- client/src/pages/ExerciseDetail.jsx
+++ client/src/pages/ExerciseDetail.jsx
@@ -151,7 +151,7 @@ export default function ExerciseDetail() {
                       onClick={() => {
                         setShowModal(true);
                       }}
-                      className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                      className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-red-700"
                     >
                       Delete
                     </a>
