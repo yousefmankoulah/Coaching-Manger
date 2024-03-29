61a8eebacb422bbb753aa8ee030c9e56f374c251
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-03-29 12:47:00 +0000

    u

--- client/src/pages/ExerciseDetail.jsx
+++ client/src/pages/ExerciseDetail.jsx
@@ -142,7 +142,7 @@ export default function ExerciseDetail() {
                 {currentUser?.role === "coach" && (
                   <div className="flex mt-4 md:mt-6">
                     <a
-                      href={`/update-customer/${currentUser._id}/${formData._id}`}
+                      href={`/ExerciseUpdate/${currentUser._id}/${formData._id}`}
                       className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                     >
                       Edit
@@ -154,12 +154,6 @@ export default function ExerciseDetail() {
                       className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                     >
                       Delete
-                    </a>
-                    <a
-                      href="#"
-                      className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                    >
-                      Message
                     </a>
                   </div>
                 )}
