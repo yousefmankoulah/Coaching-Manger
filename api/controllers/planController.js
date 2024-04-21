5e3f1ea7d3ab4e9b0085aec2c11f4d39812b84bd
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-04-21 03:13:20 +0000

    u

--- api/controllers/planController.js
+++ api/controllers/planController.js
@@ -1,4 +1,7 @@
 import { User, Plan, Subscribe } from "../models/userModel.js";
+import Stripe from "stripe";
+
+const stripe = Stripe(process.env.STRIPE_PRIVATE_KEY);
 
 export const plans = async (req, res, next) => {
   try {
@@ -6,5 +9,99 @@ export const plans = async (req, res, next) => {
     res.json(plans);
   } catch (err) {
     next(err);
+  }
+};
+
+/*********** create subscription ************/
+
+const stripeSession = async (plan) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: plan,
+          quantity: 1,
+        },
+      ],
+      success_url:
+        "https://cautious-journey-5xx4666q445cvjp5-5173.app.github.dev/success",
+      cancel_url:
+        "https://cautious-journey-5xx4666q445cvjp5-5173.app.github.dev/cancel",
+    });
+    return session;
+  } catch (e) {
+    return e;
+  }
+};
+
+export const createSubscription = async (req, res, next) => {
+  const { plan } = req.body;
+  let planId = null;
+  if (plan === 10) planId = "66241631bf573af430262dcc";
+  // Assuming 'basic' is an ID from your Plan model
+  else if (plan === 50) planId = "6624164ebf573af430262dcd";
+  else if (plan === 100) planId = "66241684bf573af430262dce";
+  else if (plan === 500) planId = "6624169fbf573af430262dcf";
+  else if (plan === 1000) planId = "662416b8bf573af430262dd0";
+
+  try {
+    const session = await stripeSession(planId);
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const subscription = new Subscribe({
+      user: user,
+      plan: planId,
+      startDate: new Date(),
+      endDate: moment().add(30, "days").toDate(), // Defaulting to 30 days for example
+      isActive: true,
+    });
+
+    await subscription.save();
+
+    return res.json({ session });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+/************ payment success ********/
+
+export const paymentSuccess = async (req, res, next) => {
+  const { sessionId } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+    if (session.payment_status === "paid") {
+      const subscriptionId = session.subscription;
+      try {
+        const stripeSubscription = await stripe.subscriptions.retrieve(
+          subscriptionId
+        );
+        const subscription = await Subscribe.findOne({
+          stripeSubscriptionId: subscriptionId, // Assuming this field exists in your schema
+        });
+
+        subscription.endDate = moment
+          .unix(stripeSubscription.current_period_end)
+          .toDate();
+        await subscription.save();
+
+        return res.json({ message: "Payment successful" });
+      } catch (error) {
+        console.error("Error retrieving subscription:", error);
+        return res.status(500).json({ error: "Error retrieving subscription" });
+      }
+    } else {
+      return res.json({ message: "Payment failed" });
+    }
+  } catch (error) {
+    res.status(500).send(error);
   }
 };
--- api/routes/planRoute.js
+++ api/routes/planRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { plans } from "../controllers/planController.js";
+import { createSubscription, paymentSuccess, plans } from "../controllers/planController.js";
 
 const router = express.Router();
 
 router.get("/plans", plans);
+router.post("/create-subscription-checkout-session", createSubscription);
+router.post("/payment-success", paymentSuccess);
+
 
 export default router;
