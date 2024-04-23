61ca99238d28776d6f2db6fc0a24b0d99d7a53da
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2024-04-23 17:20:16 +0000

    u

--- client/src/pages/Plans.jsx
+++ client/src/pages/Plans.jsx
@@ -22,13 +22,13 @@ export default function Plans() {
   return (
     <div className="min-h-screen mt-20">
       <h1 className="text-5xl font-extrabold mt-10 mb-10 text-center">Plans</h1>
-      <div className="grid grid-flow-col lg:grid-rows-2 md:grid-rows-2 sm:grid-rows-3 xs:grid-rows-6 gap-2 justify-center mt-10">
+      <div className="grid grid-flow-col lg:grid-rows-2 md:grid-rows-3 sm:grid-rows-3 xs:grid-rows-6 gap-2 justify-center mt-10">
         {formData &&
           formData.length > 0 &&
           formData.map((plan) => (
             <div
               key={plan._id}
-              className="bg-white shadow-md rounded-lg p-4 w-full"
+              className="bg-white shadow-md rounded-lg p-6 xl:w-96 lg:w-90 md-80" // Increased padding and width
             >
               <h3 className="text-xl font-bold text-center">{plan.name}</h3>
               <p>The Plan Price: ${plan.price}</p>
